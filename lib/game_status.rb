4f5600916118faeec8781ae064a247418ee1c93e
Patrick Pierre <patrick.pierre000@gmail.com>
2020-06-12 00:20:07 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -73,7 +73,7 @@ end
 def winner(board_array)
   if won?(board_array) && board_array.include?("X")
     return "X"
-  elsif won?(board_array) && board_array.include?("O")
+  if won?(board_array) && board_array.include?("O")
     return "O"
   else
     return nil
